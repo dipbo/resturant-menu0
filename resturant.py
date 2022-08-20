1283f032a2dcd10bba76f64256539e2b0d37039a
dipbo <55013052+dipbo@users.noreply.github.com>
2022-08-20 20:37:59 +0200

    Add files via upload

---
+++ resturant.py
@@ -0,0 +1,21 @@
+import time 
+menu = "pizza,burger,sandwich,coke,all"
+name = "ayan"
+print("wellcome to tekro eat.\n")
+input("can i have your name sir?\n\n")
+print("\n\nhello "+ name + " here's our menu...\n\n"
++"1.pizza\n"
++"2.burger\n"
++"3.sandwich\n"
++"4.coke\n"
++"5.all")
+order = input("so what do you want?\n\n")
+time.sleep(1)
+print("\n\nAlright, your " + order + " will be ready just in a momment.\n")
+mini = 12
+mex = 40
+input("Did you enjoy our service?\n\n")
+input("wanna tip us?\n")
+
+if 40 > 12:
+	print("thank you sir")
